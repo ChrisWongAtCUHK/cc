a119711aae5e72c6645604f6084ac5fc66d8500d
ChrisWongAtCUHK <wc38.wong@gmail.com>
2013-08-01 11:43:18 +0800

    org.springframework.aop.support.NameMatchMethodPointcutAdvisor

---
+++ Gossip/SpringGossip/NameMatchMethodPointcutAdvisor/src/cuhk/cse/HandsomeSpeaker.java
@@ -0,0 +1,20 @@
+package cuhk.cse;
+
+public class HandsomeSpeaker implements IHandsome {
+
+	public void handsome(String name){
+        // 程式主要功能
+        System.out.println(name + " is handsome");
+	}
+	
+	public void handsomeBoy(String name){
+        // 程式主要功能
+        System.out.println(name + " is a handsome boy");
+	}
+	
+	public void handsomeGirl(String name){
+        // 程式主要功能
+        System.out.println(name + " is a handsome girl");
+	}
+	
+}
---
+++ Gossip/SpringGossip/NameMatchMethodPointcutAdvisor/src/demo/SpringAOPDemo.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import cuhk.cse.*;
+
+// http://openhome.cc/Gossip/SpringGossip/AfterAdvice.html
+public class SpringAOPDemo {
+	public static void main(String[] args){
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans-config.xml");
+		
+		IHandsome handsomeProxy = (IHandsome)context.getBean("handsomeProxy");
+		handsomeProxy.handsome("Chris Wong");
+		handsomeProxy.handsomeBoy("Chris Wong");
+		handsomeProxy.handsomeGirl("Cally");
+	}
+}
